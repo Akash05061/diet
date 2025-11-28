73e01833ceedd00f67d834378daa37a578d93cbf
Akash Suttakoti <asuttakoti@gmail.com>
2025-11-28 20:00:47 +0530

    Delete src/test/java/com/dietmaker/repository directory

--- src/test/java/com/dietmaker/repository/MealRepository.java
+++
@@ -1,5 +0,0 @@
-package com.dietmaker;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MealRepository extends JpaRepository<Meal, Long> {}
--- src/test/java/com/dietmaker/repository/UserRepository.java
+++
@@ -1,5 +0,0 @@
-package com.dietmaker;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {}
